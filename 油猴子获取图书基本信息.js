42ad8d4576a3d852fb5ead92b26103888d3a5980
qiu6406 <qiu__hui01@126.com>
2019-05-15 21:16:51 +0800

    Update 油猴子获取图书基本信息.js
    增加网站判断

--- 油猴子获取图书基本信息.js
+++ 油猴子获取图书基本信息.js
@@ -16,21 +16,26 @@
 	//页面完全加载完成后
 	window.onload = (event) => {
 		var tempStr,title,author,pub,isbn,price;
+		var url = location.href;
+		if(url.indexOf("dangdang.com")>0){
+			'当当网'
+			title = document.getElementsByTagName("h1")[0].innerText;
+			author =document.getElementById("author").innerText.substring(3);
+			pub = document.getElementsByClassName("t1")[1].innerText.substring(4);
+			tempStr = document.getElementsByClassName("key clearfix").item(0).innerHTML;
+			isbn = tempStr.substring(tempStr.indexOf("国际标准书号ISBN：")+11,tempStr.indexOf("国际标准书号ISBN：")+24);
+			price = document.getElementById("original-price").innerText;
+			alert(title+","+author+","+pub+","+isbn+","+price);
+		};
 
-		'当当网'
-		title = document.getElementsByTagName("h1")[0].innerText;
-		author =document.getElementById("author").innerText.substring(3);
-		pub = document.getElementsByClassName("t1")[1].innerText.substring(4);
-		tempStr = document.getElementsByClassName("key clearfix").item(0).innerHTML;
-		isbn = tempStr.substring(tempStr.indexOf("国际标准书号ISBN：")+11,tempStr.indexOf("国际标准书号ISBN：")+24);
-		price = document.getElementById("original-price").innerText;
-		alert(title+","+author+","+pub+","+isbn+","+price);
-
-		'京东'
-		title = document.getElementsByClassName("sku-name")[0].innerText;
-		author = document.getElementById("p-author").innerText;
-		price = document.getElementById("page_maprice").innerText;
-		pub = document.getElementById("parameter2").children[0].title;
-		isbn = document.getElementById("parameter2").children[1].title;
-	}
+		if(url.indexOf("jd.com")>0){
+			'京东'
+			title = document.getElementsByClassName("sku-name")[0].innerText;
+			author = document.getElementById("p-author").innerText;
+			price = document.getElementById("page_maprice").innerText;
+			pub = document.getElementById("parameter2").children[0].title;
+			isbn = document.getElementById("parameter2").children[1].title;
+            alert(title+","+author+","+pub+","+isbn+","+price);
+		};
+	};
 })();
