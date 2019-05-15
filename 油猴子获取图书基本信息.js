dbb67d6b6d5e6ecb01dc605b85d9c264fca5d6c1
qiu6406 <404566950@qq.com>
2019-05-15 16:54:32 +0800

    init

---
+++ 油猴子获取图书基本信息.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         获取图书基本信息
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       404566950@qq.com
+// @match        http://*.dangdang.com/*
+// @grant        MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Your code here...
+    var tempStr,title,author,pub,isbn,price;
+
+    '当当网'
+    title = document.getElementsByTagName("h1")[0].innerText;
+    author =document.getElementById("author").innerText.substring(3);
+    pub = document.getElementsByClassName("t1")[1].innerText.substring(4);
+    tempStr = document.getElementsByClassName("key clearfix").item(0).innerHTML;
+    isbn = tempStr.substring(tempStr.indexOf("国际标准书号ISBN：")+11,tempStr.indexOf("国际标准书号ISBN：")+24);
+    price = document.getElementById("original-price").innerText;
+    alert(title+","+author+","+pub+","+isbn+","+price);
+
+    '京东'
+
+})();
