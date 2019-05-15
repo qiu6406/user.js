ab89950e6bb25a6e6b5f78ad964df7f842b9b9f2
qiu6406 <qiu__hui01@126.com>
2019-05-15 20:48:53 +0800

    Update 油猴子获取图书基本信息.js

--- 油猴子获取图书基本信息.js
+++ 油猴子获取图书基本信息.js
@@ -5,23 +5,31 @@
 // @description  try to take over the world!
 // @author       404566950@qq.com
 // @match        http://*.dangdang.com/*
+// @match        https://*.dangdang.com/*
+// @match        https://*.jd.com/*
 // @grant        MIT
 // ==/UserScript==
 
 (function() {
     'use strict';
     // Your code here...
-    var tempStr,title,author,pub,isbn,price;
+	window.onload = (event) => {
+		var tempStr,title,author,pub,isbn,price;
 
-    '当当网'
-    title = document.getElementsByTagName("h1")[0].innerText;
-    author =document.getElementById("author").innerText.substring(3);
-    pub = document.getElementsByClassName("t1")[1].innerText.substring(4);
-    tempStr = document.getElementsByClassName("key clearfix").item(0).innerHTML;
-    isbn = tempStr.substring(tempStr.indexOf("国际标准书号ISBN：")+11,tempStr.indexOf("国际标准书号ISBN：")+24);
-    price = document.getElementById("original-price").innerText;
-    alert(title+","+author+","+pub+","+isbn+","+price);
+		'当当网'
+		title = document.getElementsByTagName("h1")[0].innerText;
+		author =document.getElementById("author").innerText.substring(3);
+		pub = document.getElementsByClassName("t1")[1].innerText.substring(4);
+		tempStr = document.getElementsByClassName("key clearfix").item(0).innerHTML;
+		isbn = tempStr.substring(tempStr.indexOf("国际标准书号ISBN：")+11,tempStr.indexOf("国际标准书号ISBN：")+24);
+		price = document.getElementById("original-price").innerText;
+		alert(title+","+author+","+pub+","+isbn+","+price);
 
-    '京东'
-
+		'京东'
+		title = document.getElementsByClassName("sku-name")[0].innerText;
+		author = document.getElementById("p-author").innerText;
+		price = document.getElementById("page_maprice").innerText;
+		pub = document.getElementById("parameter2").children[0].title;
+		isbn = document.getElementById("parameter2").children[1].title;
+	}
 })();
